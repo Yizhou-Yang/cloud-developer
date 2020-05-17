7a452f2650b1284ae0d0cb3c71b681d8395452fc
Yizhou Yang <32808678+Yizhou-Yang@users.noreply.github.com>
2020-05-16 23:18:35 -0400

    frontend debugged

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/users/routes/auth.router.ts
@@ -22,9 +22,9 @@ async function comparePasswords(plainTextPassword: string, hash: string): Promis
     return await bcrypt.compareSync(plainTextPassword,hash);
 }
 
+
 function generateJWT(user: User): string {
-    //@TODO Use jwt to create a new JWT Payload containing
-    return jwt.sign(user,config.dev.secret);
+  return jwt.sign(user.toJSON(), config.dev.secret);
 }
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
